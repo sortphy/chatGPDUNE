d5062265011bec8ace7bc0bf6843375d6fd9384b
CodyKoInABox <codykoinabox@gmail.com>
2025-06-26 12:41:54 -0300

    update typing colors

--- ChatGPDune/frontend/chatgpdune/src/components/TypingAnimation.js
+++ ChatGPDune/frontend/chatgpdune/src/components/TypingAnimation.js
@@ -1,9 +1,9 @@
 export default function TypingAnimation() {
   return (
     <div className="flex gap-1">
-      <div className="w-2 h-2 bg-[#d97706] rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-      <div className="w-2 h-2 bg-[#d97706] rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-      <div className="w-2 h-2 bg-[#d97706] rounded-full animate-bounce"></div>
+      <div className="w-2 h-2 bg-[#8b4513] rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+      <div className="w-2 h-2 bg-[#8b4513] rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+      <div className="w-2 h-2 bg-[#8b4513] rounded-full animate-bounce"></div>
     </div>
   );
 }
