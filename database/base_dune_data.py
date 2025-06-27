e795f7b5ed8cdfc95d73eb51ba67b11f2ee1565d
CodyKoInABox <codykoinabox@gmail.com>
2025-06-27 16:14:31 -0300

    change the way data is stored

--- database/base_dune_data.py
+++ database/base_dune_data.py
@@ -1,6 +1,6 @@
 dune_data = [
-    {"from": "Paul Atreides", "type": "Character", "rel": "BELONGS_TO", "to": "House Atreides", "to_type": "House"},
-    {"from": "Baron Harkonnen", "type": "Character", "rel": "BELONGS_TO", "to": "House Harkonnen", "to_type": "House"},
-    {"from": "House Harkonnen", "type": "House", "rel": "CONTROLS", "to": "Arrakis", "to_type": "Planet"},
-    {"from": "Paul Atreides", "type": "Character", "rel": "VISITED", "to": "Arrakis", "to_type": "Planet"},
+    "Paul Atreides belongs to House Atreides.",
+    "Baron Harkonnen belongs to House Harkonnen.",
+    "House Harkonnen controls Arrakis.",
+    "Paul Atreides visited Arrakis."
 ]
